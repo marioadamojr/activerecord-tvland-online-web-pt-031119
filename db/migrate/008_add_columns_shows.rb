d0568dbd9eea8786eaf238c1469e60f09836bd61
Mario Adamo Jr <marioadamojr@gmail.com>
2019-06-03 22:10:52 +0000

    Automatically backed up by Learn

--- db/migrate/008_add_columns_shows.rb
+++ db/migrate/008_add_columns_shows.rb
@@ -2,5 +2,6 @@ class AddColumnsShows < ActiveRecord::Migration[5.2]
   def change
     add_column :shows, :genre, :string
     add_column :shows, :day, :string
+    add_column :shows, :season, :string
   end
 end
